65bb54204e6a702d84ca3783dc40a55b45fae905
monika rajpoot <rajpootmonika08@gmail.com>
2026-04-15 01:05:21 +0530

    second try

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 //this file is responsible for connecting to the database
 const mongoose = require('mongoose');
-//define url for mongodb connection
-// connection bw database and server
-// it is the mandatory step to connect to the database before performing any database operations
-const mongoURL = 'mongodb://localhost:27017/hotels';
+require('dotenv').config();
+const mongoURL= process.env.MONGODB_URL; // ye line h jo .env file me se DB_URL_LOCAL variable ko access krti h aur usse MONGODB_URL_LOCAL variable me store krti h and if DB_URL_LOCAL variable is not defined then it will use the DB_URL variable;
 mongoose.connect(mongoURL, { 
+    
      });
 //mongoose.connection object ko db variable me store krliya h taaki hum uske through connection events ko handle kr sake
-// connection events ko handle krne ke liye hum db variable ka use krte h and to interact with the database
+// connection  events ko handle krne ke liye hum db variable ka use krte h and to interact with the database
 const db=mongoose.connection;
     // event listeners for successful connection or error
     // ye event listeners h jo connection events ko handle krte h
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express'); //express module ko import krra h
 
 const app = express(); 
-// body parser middleware ko use krna h taki hum post request me data bhej sake
+const db = require('./db'); 
+require('dotenv').config();
 const bodyParser = require('body-parser');
+ // ye line h jo .env file ko load krti h taki hum usme se environment variables ko access kr sake
 app.use(bodyParser.json()); // ye middleware h jo post request me data ko json format me convert krta h 
+const PORT = process.env.PORT || 3000; 
 // database connection
-const db = require('./db');
+
+// ye line h jo .env file ko load krti h taki hum usme se environment variables ko access kr sake
+
 app.get('/', (req, res) => {
   res.send('welcome to our hotel') //response me ye milega 
 })
@@ -21,11 +26,12 @@ const menuRoutes = require('./routes/menuRoutes');
 // use the routers
 app.use('/menu', menuRoutes); 
 // ye middleware h jo menuRoutes ko use krta h
-  
+
+// ye line h jo .env file me se PORT variable ko access krti h aur agar wo variable define nahi h to default port 3000 use krti h
 
 // jb koi /menu pr get request bhejega to usse ye milega  
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('listening on port 3000') // jb server successfully start ho jata h to ye message print hota h
   console.log('Server is running on http://localhost:3000')
 })
