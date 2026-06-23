c27cad1f521d8139aec9717234ce46954bba0260
Monika Rajpoot <rajpootmonika08@gmail.com>
2026-06-23 19:21:34 +0530

    Remove unnecessary comments in server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express'); //express module ko import krra h
+const express = require('express'); 
 
 const app = express(); 
 const db = require('./db'); 
 const bodyParser = require('body-parser');
- // ye line h jo .env file ko load krti h taki hum usme se environment variables ko access kr sake
-app.use(bodyParser.json()); // ye middleware h jo post request me data ko json format me convert krta h 
+ 
+app.use(bodyParser.json()); 
 
 
 app.get('/', (req, res) => {
@@ -20,7 +20,7 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/menu', menuRoutes); 
 
 app.listen(3000, () => {
-  console.log('listening on port 3000') // jb server successfully start ho jata h to ye message print hota h
+  console.log('listening on port 3000') 
   console.log('Server is running on http://localhost:3000')
 })
 
