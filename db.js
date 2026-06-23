9d74083900ee76f016bf2c250ba41d88335e78c4
Monika Rajpoot <rajpootmonika08@gmail.com>
2026-06-23 19:23:35 +0530

    Update db.js
    Removed commented lines explaining error and disconnect events.

--- db.js
+++ db.js
@@ -10,15 +10,15 @@ db.once('connected', function() {
       console.log('Connected to port:', db.port);
       console.log('Connected to database:', db.name);
     }); 
-    //similarly, agar connection me koi error aata h to ye event trigger hota h and uske baad hum console me error message print krte h
+ 
 db.on('error', function(err) {
       console.log('Error connecting to MongoDB:', err);
     });
-    // agar connection disconnect ho jata h to ye event trigger hota h and uske baad hum console me message print krte h
+ 
 db.on('disconnected', function() {
       console.log('Disconnected from MongoDB');
     });
-    // to connect to the database, we need to export the db variable so that we can use it in other files like server.js
+ 
 module.exports = db;
 
      
